672424d62857250996bb4f52dd0f274aad2646f3
SMoTra192 <m.golub2002@gmail.com>
2023-02-10 22:44:24 +0300

    PlayerMovementJoystick

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float playerSpeed;
+    void Update()
+    {
+        _rigidbody.velocity = new Vector3(_joystick.Horizontal * playerSpeed, _rigidbody.velocity.y,
+            _joystick.Vertical * playerSpeed);
+    }
+}
